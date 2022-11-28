210eaa64e687ab68dd426bcc3bd3d5ff79587209
ARTWORKHEX <112499761+artworkhex@users.noreply.github.com>
2022-11-28 13:52:35 +0700

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
-/** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
-'./index.html',
-'./**/*.html',
+    './index.html',
+    './**/*.html',
   ],
   theme: {
+    screens: {
+      sm1: "480px",
+      sm2: "600x",
+      md1: "788px",
+      md2: "888px",
+      lg1: "976px",
+      lg2: "1276px",
+      xl: "1440px",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
