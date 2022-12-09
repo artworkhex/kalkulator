494d0b90da96e2bca5f8ac686a36a0cf33fee8de
ARTWORKHEX <112499761+artworkhex@users.noreply.github.com>
2022-12-10 01:53:56 +0700

    Add files via upload

---
+++ pertanyaan/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './index.html',
+    './**/*.html',
+  ],
+  theme: {
+    screens: {
+      sm: "380px",
+      md: "688px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
