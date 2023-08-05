5fb22342ea17036fc84ae7769e1aabf28e05dbfe
artworkhex <artworkhex@gmail.com>
2023-08-05 22:15:05 +0700

    update

--- src/script.js
+++ src/script.js
@@ -1,4 +1,7 @@
 
+
+alert('game bebas virus dan terjanin keamanannya, untuk usia 9+, tidak support android')
+alert("contact me on instagram, @zzls__  i'm programer")
 const swiper = new Swiper('.swiper', {
 
     // If we need pagination
